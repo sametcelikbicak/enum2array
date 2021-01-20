369df221d963e770eb2eb9e2a37bc5b228136574
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-01-20 16:19:45 +0300

    Add tslint and prettier

--- src/array-value.ts
+++ src/array-value.ts
@@ -1,14 +1,14 @@
 export interface IArrayValue {
-    readonly title: string;
-    readonly value: any;
+  readonly title: string;
+  readonly value: any;
 }
 
 export class ArrayValue implements IArrayValue {
-    public readonly title: string;
-    public readonly value: any;
+  public readonly title: string;
+  public readonly value: any;
 
-    constructor(title: string = "", value: any = "") {
-        this.title = title;
-        this.value = value;
-    }
+  constructor(title: string = '', value: any = '') {
+    this.title = title;
+    this.value = value;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import { ArrayValue, IArrayValue } from "./array-value";
+import { ArrayValue, IArrayValue } from './array-value';
 
 export function enum2array(enumObject: any): IArrayValue[] {
-    if (Object.keys(enumObject).some((value) => Number(value))) {
-        return Object.keys(enumObject)
-            .filter((value) => isNaN(Number(value)) === false)
-            .map((key) => new ArrayValue(enumObject[key], Number(key)));
-    }
+  if (Object.keys(enumObject).some((value) => Number(value))) {
+    return Object.keys(enumObject)
+      .filter((value) => isNaN(Number(value)) === false)
+      .map((key) => new ArrayValue(enumObject[key], Number(key)));
+  }
 
-    return Object.keys(enumObject).map(
-        (key) => new ArrayValue(key, enumObject[key])
-    );
+  return Object.keys(enumObject).map((key) => new ArrayValue(key, enumObject[key]));
 }
