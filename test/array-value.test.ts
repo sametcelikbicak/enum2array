aeb86410422b3d4e38811fcd53b937da2252497a
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-07-02 15:22:00 +0300

    Change jest config and fix failing tests

--- test/array-value.test.ts
+++ test/array-value.test.ts
@@ -1,15 +1,15 @@
-import {ArrayValue} from "../src/array-value";
+import { ArrayValue } from '../src/array-value';
 
-describe("ArrayValue", () => {
-    it("should be defined", () => {
-      expect(ArrayValue).toBeDefined();
-    });
+describe('ArrayValue', () => {
+  it('should be defined', () => {
+    expect(ArrayValue).toBeDefined();
+  });
 
-    it("should match valid object", () => {
-      const expected = {title: "TypeA", value: 1};
-      
-      const result = new ArrayValue("TypeA", 1);
+  it('should match valid object', () => {
+    const expected = { title: 'TypeA', value: 1 };
 
-      expect(result).toEqual(expected);
+    const result = new ArrayValue('TypeA', 1);
+
+    expect(result).toEqual(expected);
   });
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,48 +1,48 @@
-import { ArrayValue } from "../src/array-value";
-import { enum2array } from "../src/index";
+import { ArrayValue } from '../src/array-value';
+import { enum2array } from '../src/index';
 
 enum CustomType {
-    TypeA = 1,
-    TypeB = 2,
-    TypeC = 3,
-    TypeD = 4,
+  TypeA = 1,
+  TypeB = 2,
+  TypeC = 3,
+  TypeD = 4,
 }
 
 enum CustomValue {
-  TypeA = "valueA",
-  TypeB = "valueB",
-  TypeC = "valueC",
-  TypeD = "valueD",
+  TypeA = 'valueA',
+  TypeB = 'valueB',
+  TypeC = 'valueC',
+  TypeD = 'valueD',
 }
 
-describe("enum2array", () => {
-    it("should be defined", () => {
-        expect(enum2array).toBeDefined();
-    });
+describe('enum2array', () => {
+  it('should be defined', () => {
+    expect(enum2array).toBeDefined();
+  });
 
-    it("should match valid array with number value", () => {
-        const expected = [
-            new ArrayValue("TypeA", 1),
-            new ArrayValue("TypeB", 2),
-            new ArrayValue("TypeC", 3),
-            new ArrayValue("TypeD", 4),
-        ];
+  it('should match valid array with number value', () => {
+    const expected = [
+      new ArrayValue('TypeA', 1),
+      new ArrayValue('TypeB', 2),
+      new ArrayValue('TypeC', 3),
+      new ArrayValue('TypeD', 4),
+    ];
 
-        const result = enum2array(CustomType);
+    const result = enum2array(CustomType);
 
-        expect(result).toEqual(expected);
-    });
+    expect(result).toEqual(expected);
+  });
 
-    it("should match valid array with string value", () => {
-      const expected = [
-          new ArrayValue("TypeA", "valueA"),
-          new ArrayValue("TypeB", "valueB"),
-          new ArrayValue("TypeC", "valueC"),
-          new ArrayValue("TypeD", "valueD"),
-      ];
+  it('should match valid array with string value', () => {
+    const expected = [
+      new ArrayValue('TypeA', 'valueA'),
+      new ArrayValue('TypeB', 'valueB'),
+      new ArrayValue('TypeC', 'valueC'),
+      new ArrayValue('TypeD', 'valueD'),
+    ];
 
-      const result = enum2array(CustomValue);
+    const result = enum2array(CustomValue);
 
-      expect(result).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 });
