912b776731edc69232f3820b197651157c2f5f23
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-09-06 11:58:11 +0300

    Update yml and script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,8 @@
 GIT_COMMIT=$(git rev-parse --short HEAD)
 GIT_TAG=$(git describe --tags --exact-match $COMMIT || :)
 
-echo ${GIT_TAG}
-
 if [[ ${GIT_TAG} == v* ]] ; then
-    echo "Tag found..."
+    echo "$GIT_TAG Tag found..."
 else
     echo "No git tag found, action cancelled..."
     exit 1
